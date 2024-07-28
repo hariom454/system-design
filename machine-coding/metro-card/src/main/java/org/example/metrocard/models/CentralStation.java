3ad1c0beed756c46be4b354b110831cff4103c94
hariom454 <thakurhariomsingh498@gmail.com>
2024-07-28 17:15:02 +0530

    add abstraction and removed duplicate code for metro station implementation.

--- machine-coding/metro-card/src/main/java/org/example/metrocard/models/CentralStation.java
+++ machine-coding/metro-card/src/main/java/org/example/metrocard/models/CentralStation.java
@@ -1,80 +1,11 @@
 package org.example.metrocard.models;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import org.example.metrocard.constants.MetroConstants;
-import org.example.metrocard.utils.Pair;
 
-public class CentralStation implements MetroStation {
-
-  private int total;
-  private int serviceFee;
-  private int discount;
-  private final List<Journey> journeys;
-
-  public CentralStation() {
-    this.journeys = new ArrayList<>();
-  }
-
-  @Override
-  public int getTotalCollection() {
-    return this.total + this.serviceFee;
-  }
-
-  @Override
-  public int getTotalDiscount() {
-    return this.discount;
-  }
-
-  private void updateTotalCollection(int total) {
-    this.total += total;
-  }
-
-  private void updateDiscount(int amount) {
-    this.discount += amount;
-  }
-
-  private void updateServiceCharges(int amount) {
-    this.serviceFee += amount;
-  }
-
-  @Override
-  public List<Pair> getPassengerSummary() {
-    Map<String, Integer> mp = new HashMap<>();
-    for (Journey j : journeys) {
-      Passenger p = j.getPassenger();
-      String type = p.getUserType();
-      int cnt = mp.getOrDefault(type, 0) + 1;
-      mp.put(type, cnt);
-    }
-    List<Pair> pas = new ArrayList<>();
-    mp.forEach((key1, value1) -> pas.add(new Pair(key1, value1)));
-    pas.sort((first, second) -> {
-      if (first.getValue() == second.getValue()) {
-        return first.getKey().compareTo(second.getKey());
-      }
-      return second.getValue() - first.getValue();
-    });
-    return pas;
-  }
-
-  @Override
-  public void checkin(Journey j) {
-    journeys.add(j);
-    updateCharges(j);
-  }
+public class CentralStation extends MetroStation {
 
   @Override
   public String getStationName() {
     return MetroConstants.CENTRAL_STATION;
-  }
-
-  private void updateCharges(Journey j) {
-    Charges c = j.getCharges();
-    updateTotalCollection(c.getFare());
-    updateDiscount(c.getDiscount());
-    updateServiceCharges(c.getServiceFee());
   }
 }
--- machine-coding/metro-card/src/main/java/org/example/metrocard/models/MetroStation.java
+++ machine-coding/metro-card/src/main/java/org/example/metrocard/models/MetroStation.java
@@ -1,19 +1,74 @@
 package org.example.metrocard.models;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.example.metrocard.utils.Pair;
 
-public interface MetroStation {
+public abstract class MetroStation {
 
-  int getTotalCollection();
+  private int total;
+  private int serviceFee;
+  private int discount;
+  private final List<Journey> journeys;
 
-  int getTotalDiscount();
+  public MetroStation() {
+    this.journeys = new ArrayList<>();
+  }
 
-  List<Pair> getPassengerSummary();
+  public int getTotalCollection() {
+    return this.total + this.serviceFee;
+  }
 
-  void checkin(Journey j);
+  public int getTotalDiscount() {
+    return this.discount;
+  }
 
-  String getStationName();
+  private void updateTotalCollection(int total) {
+    this.total += total;
+  }
+
+  private void updateDiscount(int amount) {
+    this.discount += amount;
+  }
+
+  private void updateServiceCharges(int amount) {
+    this.serviceFee += amount;
+  }
+
+  public List<Pair> getPassengerSummary() {
+    Map<String, Integer> mp = new HashMap<>();
+    for (Journey j : journeys) {
+      Passenger p = j.getPassenger();
+      String type = p.getUserType();
+      int cnt = mp.getOrDefault(type, 0) + 1;
+      mp.put(type, cnt);
+    }
+    List<Pair> pas = new ArrayList<>();
+    mp.forEach((key1, value1) -> pas.add(new Pair(key1, value1)));
+    pas.sort((first, second) -> {
+      if (first.getValue() == second.getValue()) {
+        return first.getKey().compareTo(second.getKey());
+      }
+      return second.getValue() - first.getValue();
+    });
+    return pas;
+  }
+
+  public void checkin(Journey j) {
+    journeys.add(j);
+    updateCharges(j);
+  }
+
+  public abstract String getStationName();
+
+  private void updateCharges(Journey j) {
+    Charges c = j.getCharges();
+    updateTotalCollection(c.getFare());
+    updateDiscount(c.getDiscount());
+    updateServiceCharges(c.getServiceFee());
+  }
 }
 
 
